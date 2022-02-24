692633f452feddcb98542d955817ea2b184aa119
Your Name <you@example.com>
2022-02-23 19:51:50 -0500

    FBA-207: updated build.sh

--- FBA-207/build.sh
+++ FBA-207/build.sh
@@ -1,1 +1,3 @@
-hipcc code.hip -c -o code.o -Xclang -debug-info-kind=standalone -fdebug-types-section
+# install rocm4.5.2
+# build and install rocThrust: cd rocThrust ; mkdir cd build ; cd build ; cmake .. ; make -j8 install
+hipcc code.hip -std=c++14 -I/opt/rocm/include -c -o code.o -Xclang -debug-info-kind=standalone -fdebug-types-section
