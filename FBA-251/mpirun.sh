2250eec16c0232d758072841a5611a8b18859f13
Your Name <you@example.com>
2022-08-04 12:50:28 -0400

    mpirun.sh: enhancement runtime measured

--- FBA-251/mpirun.sh
+++ FBA-251/mpirun.sh
@@ -1,6 +1,11 @@
-
-PROCESSESS=8
+PROCESSESS=512
 MPIRUN=`sudo find /usr/ -name mpirun`
+# MinSi's, simple call to hipGetDeviceCont
+APP_PATH=./a.out
+
+# hip-examples, hello world, multitide of HIP API calls.
+APP_PATH=/home/AMD/gg/ROCm-5.2/HIP-Examples/HIP-Examples-Applications/HelloWorld/a.out
+
 if [[ -z $MPIRUN ]] ; then
     echo "Unable to find mpirun. Will try installing openmpi-devel..."
     sudo yum install openmpi-devel -y
@@ -11,9 +16,13 @@ if [[ -z $MPIRUN ]] ; then
     fi
 fi
 
+t1=$SECONDS
 
-APP_PATH=./a.out
 $MPIRUN --allow-run-as-root -np $PROCESSESS -N $PROCESSESS --oversubscribe $APP_PATH
+
+t2=$SECONDS 
+echo "total time for $PROCESSESS processes: "
+echo $((t2-t1))
 
 
 
