91d4069fbe786e44d991ea424f825e4e8fd42851
Your Name <you@example.com>
2022-08-04 12:13:15 -0400

    mpirun.sh: made it more robust

--- FBA-251/mpirun.sh
+++ FBA-251/mpirun.sh
@@ -1,5 +1,17 @@
+
 PROCESSESS=8
-MPIRUN=/usr/lib64/openmpi/bin/mpirun
+MPIRUN=`sudo find /usr/ -name mpirun`
+if [[ -z $MPIRUN ]] ; then
+    echo "Unable to find mpirun. Will try installing openmpi-devel..."
+    sudo yum install openmpi-devel -y
+    MPIRUN=`sudo find /usr/ -name mpirun`
+    if [[ -z $MPIRUN ]] ; then
+        echo "Unable to find mpirun after installing openmpi-devel. Giving up."
+        exit 1
+    fi
+fi
+
+
 APP_PATH=./a.out
 $MPIRUN --allow-run-as-root -np $PROCESSESS -N $PROCESSESS --oversubscribe $APP_PATH
 
