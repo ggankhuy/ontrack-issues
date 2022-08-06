7da883f125773e0e5c3b41ff609bf158fdadc591
Your Name <you@example.com>
2022-08-05 17:10:37 -0700

    FBA-211.reproducer.sh: working now, but not reproducing fba-211

--- FBA-211/FBA-211.reproducer.sh
+++ FBA-211/FBA-211.reproducer.sh
@@ -5,21 +5,26 @@ mkdir -p $LOG_FOLDER
 for i in {1..10} ; do 
     echo "========================="
     echo "loop $i..."
+
     dmesg --clear
     modprobe amdgpu
-    echo "dmesg...after modprobe amdgpu..." 
+    echo "dmesg after modprobe amdgpu..." 
     dmesg | tee $LOG_FOLDER/dmesg.modprobe.amdgpu.$i.log
+
     ./gpuburn-hip 2>&1 | tee  $LOG_FOLDER/gpuburn-hip.$i.log
     dmesg | tee $LOG_FOLDER/dmesg.after.apprun.$i.log
     dmesg --clear
-    modprobe -r amdgpu | tee $LOG_FOLDER/dmesg.modprobe-r.amdgpu.$i.log
+    
+    sleep 5
+    modprobe -r amdgpu 
 
-    r=$?    
-    echo "r: $r /modprobe -r amdgpu/"
+    r=$?  
+    echo "r: $r /modprobe -r amdgpu/" 
+    
     if [[ $r -ne 0 ]] ; then
         echo "modprobe -r amdgpu returned non-zero. Exiting prematurely."
         exit 1
     fi
-    echo "dmesg...after modprobe -r amdgpu..." 
+    echo "dmesg after modprobe -r amdgpu..." 
     dmesg | tee $LOG_FOLDER/dmesg.modprobe-r.amdgpu.$i.log
 done
