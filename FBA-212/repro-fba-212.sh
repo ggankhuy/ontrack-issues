aee188095c27f407d3e4cb2d0050bffc6a8c12d1
Your Name <you@example.com>
2022-08-11 08:52:13 -0700

    FBA-212/repro-fba-212.log: removed

--- FBA-212/repro-fba-212.sh
+++ FBA-212/repro-fba-212.sh
@@ -8,11 +8,20 @@ USER=root
 
 DATE=`date +%Y%m%d-%H-%M-%S`
 OUTPUT_DIR=fba-212-$DATE
+
+#ep08
 CONFIG_IP_GUEST=10.216.64.100
 CONFIG_PORT_GUEST=30115
+
+#ixt180
+CONFIG_IP_GUEST=10.216.54.104
+CONFIG_PORT_GUEST=22
 LOOP_COUNT=3
 SINGLE_BAR="-----------------------------"
 DOUBLE_BAR="============================="
+
+mkdir -p $OUTPUT_DIR
+
 function wait_host_up() {
     sleep 3
     for i in {1..60} ; do
@@ -31,7 +40,8 @@ function wait_host_up() {
 #                "sudo dmesg | sudo tee fba-212/dmesg.after.reboot.'$loop'.log" \
 #                "/usr/local/bin/run_kfdtest.sh 2>&1 | sudo tee fba-212/$loop.kfdtest.log " \
 #                "/usr/local/bin/kfdtest --gtest_filter=-*LargestSysBuffero* 2>&1 | sudo tee fba-212/$loop.kfdtest.log " \
-#                "sudo /opt/rocm-5.0.1/rvs/rvs -c /opt/rocm-5.0.1/rvs/conf/gst_single.conf 2>&1 | sudo tee fba-212/$loop.rvs.gst.test.log" \
+#                "sudo /opt/rocm-4.3.1/rvs/rvs -c /opt/rocm-4.3.1/rvs/conf/gst_single.conf 2>&1 | sudo tee fba-212/$loop.rvs.gst.test.log" \
+#                "sudo dmesg --clear" "sudo rmmod amdgpu" "sudo dmesg | sudo tee fba-212/dmesg.after.rmmod.'$loop'.log" \
 
 wait_host_up
 for loop in {1..10} ; do
@@ -42,8 +52,8 @@ for loop in {1..10} ; do
                 "sudo dmesg --clear"  "sudo modprobe amdgpu" \
                 "sudo dmesg | sudo tee fba-212/dmesg.after.modprobe.amdgpu.'$loop'.log" \
                 "sudo dmesg --clear" \
-                "/usr/local/bin/kfdtest --gtest_filter=-*LargestSysBuffero* 2>&1 | sudo tee fba-212/$loop.kfdtest.log " \
                 "sudo dmesg | sudo tee fba-212/dmesg.after.workload.'$loop'.log" \
+                "sudo /opt/rocm-4.3.1/rvs/rvs -c /opt/rocm-4.3.1/rvs/conf/gst_single.conf 2>&1 | sudo tee fba-212/$loop.rvs.gst.test.log" \
                 "sudo dmesg --clear" "sudo rmmod amdgpu" "sudo dmesg | sudo tee fba-212/dmesg.after.rmmod.'$loop'.log" \
         ; do
         echo "$SINGLE_BAR"
