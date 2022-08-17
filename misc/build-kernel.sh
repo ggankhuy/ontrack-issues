b5d81e6a9fb710768c79a6cb13fa7a67211e3309
Your Name <you@example.com>
2022-08-17 16:01:54 -0400

    build-kernel.sh: ehnanced

--- misc/build-kernel.sh
+++ misc/build-kernel.sh
@@ -1,8 +1,14 @@
+rm -rf /home/nonroot/gg/rpm/rock-kernel/config.meta/1/*
+rm -rf /home/nonroot/gg/rpm/rock-kernel/config.meta/2/*
+
 rm -rf /root/rpmbuild/ ; 
 make clean ; make -j`nproc` binrpm-pkg
 mv /root/rpmbuild/RPMS/x86_64/kernel-*  /home/nonroot/gg/rpm/rock-kernel/config.meta/1
 ls -l /home/nonroot/gg/rpm/rock-kernel/config.meta/1
 
+rm -rf /root/rpmbuild/ ; 
 make clean ; make -j`nproc` rpm-pkg
 mv /root/rpmbuild/RPMS/x86_64/kernel-*  /home/nonroot/gg/rpm/rock-kernel/config.meta/2
-ls -l /home/nonroot/gg/rpm/rock-kernel/config.meta/1
+ls -l /home/nonroot/gg/rpm/rock-kernel/config.meta/2
+tree -f /home/nonroot/gg/rpm/rock-kernel/config.meta/
+echo "Build files stored at:  /home/nonroot/gg/rpm/rock-kernel/config.meta/"
