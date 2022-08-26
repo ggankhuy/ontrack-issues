49203ae7be8d865bf8512aab031c139cce2c8349
Your Name <you@example.com>
2022-08-26 11:37:27 -0400

    mini-test.sh: tested TB fully.

--- FBA-251/mini-reg/mini-test.sh
+++ FBA-251/mini-reg/mini-test.sh
@@ -19,7 +19,10 @@ LOG_FOLDER_RVS=$LOG_FOLDER/rvs/
 mkdir -p $LOG_FOLDER_RVS
 
 TB=`find /opt -name TransferBench`
-TB_CONFIG=./TB.8.bidi.GPU.cfg
+TB_DIRNAME=`dirname $TB`
+TB_BASENAME=`basename $TB`
+TB_CONFIG=`pwd`/TB.8.bidi.GPU.cfg
+echo "TB_DIRNAME/BASENAME/CONFIG: $TB_DIRNAME, $TB_BASENAME, $TB_CONFIG"
 LOG_FOLDER_TB=$LOG_FOLDER/tb/
 mkdir -p $LOG_FOLDER_TB
 
@@ -29,22 +32,20 @@ else
     echo "Running TB..."
     dmesg --clear
     ipmitool -H $IPMIHOST -I lanplus -U $IPMIUSER -P $IPMIPW sel clear
-    sleep 5
     dmesg -wH > $LOG_FOLDER_TB/tb.dmesg.log &
-    DMESG_PID=$!
-    mkdir $LOG_FOLDER_TB/sel -p
-    $TB $TB_CONFIG 8G | tee $LOG_FOLDER_TB/tb.log
+    echo $TB $TB_CONFIG 512M 2>&1 | tee $LOG_FOLDER_TB/tb.log
+    $TB $TB_CONFIG 512M 2>&1 | tee $LOG_FOLDER_TB/tb.log
     ipmitool -H $IPMIHOST -I lanplus -U $IPMIUSER -P $IPMIPW sel elist > $LOG_FOLDER_TB/tb.sel.log
     #kill -p $DMESG_PID
 fi
 
+exit 0
 if [[ -z $RVS ]] || [[ -z $RVS_CONFIG ]]; then
     echo "Unable to find either rvs or rvs config. RVS: $RVS, RVS_CONFIG: $RVS_CONFIG"
 else
     echo "Running RVS..."
     dmesg --clear
     ipmitool -H $IPMIHOST -I lanplus -U $IPMIUSER -P $IPMIPW sel clear
-    sleep 5
     dmesg -wH > $LOG_FOLDER_RVS/rvs.dmesg.log &
     DMESG_PID=$!
     mkdir $LOG_FOLDER_RVS/sel -p
@@ -65,7 +66,6 @@ else
     echo "Running kfdtest..."
     dmesg --clear
     ipmitool -H $IPMIHOST -I lanplus -U $IPMIUSER -P $IPMIPW sel clear
-    sleep 5
     dmesg -wH > $LOG_FOLDER_KFDTEST/run_kfdtest.dmesg.log &
     DMESG_PID=$!
     mkdir $LOG_FOLDER_KFDTEST/sel -p
