55408e64f9866805cdd7e46e3ee2de064cbde85e
Your Name <you@example.com>
2022-08-26 11:20:07 -0400

    mini-test.sh: further enhancement

--- FBA-251/mini-reg/mini-test.sh
+++ FBA-251/mini-reg/mini-test.sh
@@ -13,9 +13,30 @@ else
 fi
 
 RVS=`find /opt -name rvs`
+RVS=/opt/rocm-5.2.0/rvs/rvs
 RVS_CONFIG=`find /opt -name gst_single.conf`
 LOG_FOLDER_RVS=$LOG_FOLDER/rvs/
 mkdir -p $LOG_FOLDER_RVS
+
+TB=`find /opt -name TransferBench`
+TB_CONFIG=./TB.8.bidi.GPU.cfg
+LOG_FOLDER_TB=$LOG_FOLDER/tb/
+mkdir -p $LOG_FOLDER_TB
+
+if [[ -z $TB ]] || [[ -z $TB_CONFIG ]]; then
+    echo "Unable to find either rvs or rvs config. TB: $TB, TB_CONFIG: $TB_CONFIG"
+else
+    echo "Running TB..."
+    dmesg --clear
+    ipmitool -H $IPMIHOST -I lanplus -U $IPMIUSER -P $IPMIPW sel clear
+    sleep 5
+    dmesg -wH > $LOG_FOLDER_TB/tb.dmesg.log &
+    DMESG_PID=$!
+    mkdir $LOG_FOLDER_TB/sel -p
+    $TB $TB_CONFIG 8G | tee $LOG_FOLDER_TB/tb.log
+    ipmitool -H $IPMIHOST -I lanplus -U $IPMIUSER -P $IPMIPW sel elist > $LOG_FOLDER_TB/tb.sel.log
+    #kill -p $DMESG_PID
+fi
 
 if [[ -z $RVS ]] || [[ -z $RVS_CONFIG ]]; then
     echo "Unable to find either rvs or rvs config. RVS: $RVS, RVS_CONFIG: $RVS_CONFIG"
@@ -29,9 +50,10 @@ else
     mkdir $LOG_FOLDER_RVS/sel -p
     $RVS -c $RVS_CONFIG | tee $LOG_FOLDER_RVS/rvs.log
     ipmitool -H $IPMIHOST -I lanplus -U $IPMIUSER -P $IPMIPW sel elist > $LOG_FOLDER_RVS/rvs.sel.log
-    kill -p $DMESG_PID
+    #kill -p $DMESG_PID
 fi
 
+exit 0
 LOG_FOLDER_KFDTEST=$LOG_FOLDER/kfdtest/
 mkdir -p $LOG_FOLDER_KFDTEST
 
@@ -49,26 +71,6 @@ else
     mkdir $LOG_FOLDER_KFDTEST/sel -p
     $RUN_KFDTEST_SH | tee $LOG_FOLDER_KFDTEST/run_kfdtest.log
     ipmitool -H $IPMIHOST -I lanplus -U $IPMIUSER -P $IPMIPW sel elist > $LOG_FOLDER_KFDTEST/run_kfdtest.sel.log
-    kill -p $DMESG_PID
-fi
-
-TB=`find /opt -name TransferBench`
-TB_CONFIG=./TB.8.bidi.GPU.cfg
-LOG_FOLDER_TB=$LOG_FOLDER/rvs/
-mkdir -p $LOG_FOLDER_TB
-
-if [[ -z $TB ]] || [[ -z $TB_CONFIG ]]; then
-    echo "Unable to find either rvs or rvs config. TB: $TB, TB_CONFIG: $TB_CONFIG"
-else
-    echo "Running TB..."
-    dmesg --clear
-    ipmitool -H $IPMIHOST -I lanplus -U $IPMIUSER -P $IPMIPW sel clear
-    sleep 5
-    dmesg -wH > $LOG_FOLDER_TB/tb.dmesg.log &
-    DMESG_PID=$!
-    mkdir $LOG_FOLDER_TB/sel -p
-    $TB $TB_CONFIG 8G | tee $LOG_FOLDER_TB/tb.log
-    ipmitool -H $IPMIHOST -I lanplus -U $IPMIUSER -P $IPMIPW sel elist > $LOG_FOLDER_TB/tb.sel.log
-    kill -p $DMESG_PID
+    #kill -p $DMESG_PID
 fi
 
