a32ed30e7fa13b7f1b9f42125ea6a95ba78b1abe
Your Name <you@example.com>
2022-09-05 03:36:03 +0000

    modularize scripts in pt folder.

--- misc/pt/lib.sh
+++ misc/pt/lib.sh
@@ -38,3 +38,45 @@ function install_python() {
     cd ..
 }
 
+function install_conda() {
+    
+    # Anaconda defs.
+
+    FILE_ANACONDA=Anaconda3-2021.11-Linux-x86_64.sh
+    PREFIX_ANACONDA=/Anaconda3
+    CONFIG_UPGRADE_ANACONDA=1
+    EXEC_PATH_CONDA=$PREFIX_ANACONDA/bin/conda
+
+    wget -nc https://repo.anaconda.com/archive/$FILE_ANACONDA
+    ret=$? ; echo $ret.
+    if [[ $ret -ne 0 ]] ; then
+        echo "code: $ret. Download failure for $FILE_ANACONDA, check the url."
+        exit 1
+    fi
+    chmod  777 $FILE_ANACONDA
+    mkdir -p $PREFIX_ANACONDA
+    if [[ $CONFIG_UPGRADE_ANACONDA=="1" ]] ; then
+        echo "Installing/upgrading regardless of existing installation..."
+        ./$FILE_ANACONDA  -u -b -p $PREFIX_ANACONDA
+    else
+        echo "Installing only if it is not installed in $PREFIX_ANACONDA location..."
+        ./$FILE_ANACONDA  -b -p $PREFIX_ANACONDA
+    fi    
+
+
+    #if [[ `cat ~/.bashrc | grep PATH | grep $PREFIX_ANACONDA` ]] ; then
+    #    echo "Inserting path onto bashrc in case reboot next time. "
+    #    echo "export PATH=$PATH:$PREFIX_ANACONDA/bin" >> ~/.bashrc
+    #else
+    #    echo "conda path is already defined in bashrc."
+    #fi
+    ln -s $EXEC_PATH_CONDA /usr/bin/conda
+    echo "Testing the installation..."
+    $EXEC_PATH_CONDA
+
+    if [[ $? -ne 0 ]] ; then
+        echo "Can not find $EXEC_PATH_CONDA."
+        exit 1
+    fi  
+}
+
