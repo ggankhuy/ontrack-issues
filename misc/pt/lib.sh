144591b73885ae503f4067cb9371df249a05dfc7
Your Name <you@example.com>
2022-09-05 03:34:34 +0000

    lib.sh: added

---
+++ misc/pt/lib.sh
@@ -0,0 +1,40 @@
+function install_python() {
+    #Python defs.
+
+    PYTHON_VER_MAJOR=3.9
+    PYTHON_VER_MINOR=10
+    PYTHON_VER=$PYTHON_VER_MAJOR.$PYTHON_VER_MINOR
+    PYTHON_FULL_NAME=Python-$PYTHON_VER
+    PYTHON_TAR=$PYTHON_FULL_NAME.tgz
+
+    sudo yum -y install epel-release
+    sudo yum update -y
+    sudo yum groupinstall "Development Tools" -y
+
+    sudo yum install openssl-devel libffi-devel bzip2-devel -y
+    wget -nc https://www.python.org/ftp/python/$PYTHON_VER/$PYTHON_TAR
+    tar -xvf $PYTHON_TAR
+    cd $PYTHON_FULL_NAME
+
+    if [[ $? -ne 0 ]] ; then
+        echo "Can not cd into $PYTHON_VER directory..."
+        exit 1
+    fi
+    ./configure --enable-optimizations
+    sudo make -j`nproc` install
+
+    echo "Testing the installation..."
+    python$PYTHON_VER_MAJOR --version
+    if [[ $? -ne 0 ]] ; then
+        echo "Unable to find 3.9"
+    fi
+    PATH_PYTHON_U=`which python$PYTHON_VER_MAJOR`
+    echo "new path: $PATH_PYTHON_U"
+    rm -rf /usr/bin/python
+    echo ln -s $PATH_PYTHON_U /usr/bin/python
+    ln -s $PATH_PYTHON_U /usr/bin/python
+    rm -rf /usr/bin/python3
+    ln -s /usr/bin/python /usr/bin/python3
+    cd ..
+}
+
