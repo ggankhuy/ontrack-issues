db0074619bf7a801255efa78a21af5dbdba5006b
Your Name <you@example.com>
2023-09-26 23:57:56 -0700

    FBA-261: enhanced debug-run.sh, output csv

--- FBA/FBA-261/debug-run.sh
+++ FBA/FBA-261/debug-run.sh
@@ -1,12 +1,28 @@
 # this is a modification of linear.py so that, matrix sizes are passed from this script along ROCm debug parameters to associate these two.
+set -x
 LOG_DIR=log
 sudo mkdir -p $LOG_DIR
+csv_filename=output.csv
+
+echo -ne "" > $csv_filename
 
 for size in 512 1024 2048 4096 8192 ; do
+    curr_line=""
+    row_idx=0
     for dtype in float float16 bfloat16 ; do
-        TENSILE_DB=0x8000 python3 linear-arg.py 2>&1 | tee $LOG_DIR/linear-arg.size.$i.$i.$i.$dtype.TENSILE_DB.0x8000.log
-        python3 linear-arg.py 2>&1 | tee $LOG_DIR/linear-arg.size.$i.$i.$i.$dtype.log
+        #TENSILE_DB=0x8000 python3 linear-arg.py 2>&1 | tee $LOG_DIR/linear-arg.size.$size.$size.$size.$dtype.TENSILE_DB.0x8000.log
+        python3 linear-arg.py 2>&1 | tee $LOG_DIR/linear-arg.size.$size.$size.$size.$dtype.log
+    done
+    for dtype in float float16 bfloat16 ; do
+        tflops_curr=`cat $LOG_DIR/linear-arg.size.$size.$size.$size.$dtype.log  | grep TFLOPS |  awk '{print $2}'`
+
+        if [[ $row_idx == 2 ]]  ; then
+            echo "$tflops_curr" >> $csv_filename
+        else
+            echo -ne "$tflops_curr," >> $csv_filename
+        fi
+        row_idx=$((row_idx+1))
     done
 done
 
-
+cat $csv_filename
