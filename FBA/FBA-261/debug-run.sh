688dbd5cdc2cf397667ee08938cbef1e21ff177a
Your Name <you@example.com>
2023-09-26 18:11:02 -0700

    FBA-261: debug-run.sh: syntax error fixed

--- FBA/FBA-261/debug-run.sh
+++ FBA/FBA-261/debug-run.sh
@@ -4,7 +4,6 @@ sudo mkdir -p $LOG_DIR
 
 for size in 512 1024 2048 4096 8192 ; do
     for dtype in float float16 bfloat16 ; do
-        for transpose
         TENSILE_DB=0x8000 python3 linear-arg.py 2>&1 | tee $LOG_DIR/linear-arg.size.$i.$i.$i.$dtype.TENSILE_DB.0x8000.log
         python3 linear-arg.py 2>&1 | tee $LOG_DIR/linear-arg.size.$i.$i.$i.$dtype.log
     done
