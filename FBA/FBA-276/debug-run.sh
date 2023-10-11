aeccf6941b29afb68e5a16807cd4cbcbd7dbf4b5
Your Name <you@example.com>
2023-10-11 05:14:40 +0000

    03-matrix-multiplication.py: original is also added

--- FBA/FBA-276/debug-run.sh
+++ FBA/FBA-276/debug-run.sh
@@ -1,7 +1,11 @@
-mkdir log
+LOGDIR=log/debugL2
+mkdir -p $LOGDIR
+
+TENSILE_DB_VAL="20000"
+#for (( i = 2; i <  ;33; i++ )) ; do
 for (( i = 2; i <  33; i++ )) ; do
     echo "---- running for $i ... ----"
-    python3  03-matrix-multiplication-mod.py --size=$i 2>&1 | tee log/03-matrix-multiplication-mod.log
-    TENSILE_DB=0x8000 python3  03-matrix-multiplication-mod.py --size=$i 2>&1 | tee log/03-matrix-multiplication-mod.TENSILE.log
+    python3  03-matrix-multiplication-mod.py --size=$i 2>&1 | tee $LOGDIR/03-matrix-multiplication-mod.$((i * 128)).log
+    TENSILE_DB=0x$TENSILE_DB_VAL python3  03-matrix-multiplication-mod.py --size=$i 2>&1 | tee $LOGDIR/03-matrix-multiplication-mod.TENSILE.$TENSILE_DB_VAL.$((i*128)).log
 
 done
