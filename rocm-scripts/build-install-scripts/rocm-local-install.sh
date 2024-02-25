4ca1ac59b0e48a64633b2d0340e23b2a8400d4b1
Your Name <you@example.com>
2024-02-24 20:19:53 -0800

    rocm-local-install.sh: improved code, code complete, in test

--- rocm-scripts/build-install-scripts/rocm-local-install.sh
+++ rocm-scripts/build-install-scripts/rocm-local-install.sh
@@ -1,9 +1,27 @@
-#set -x
+set -x
+yum install createrepo -y
 for i in rocm amdgpu; do
     path=`find $i -name repodata`
     dir=`dirname $path`
     pushd $dir
     pwd
     createrepo .
+    echo -ne "" > $i.repo
+    echo "[$i]" >> $i.repo
+    echo "name=$i repository" >> $i.repo
+    echo "baseurl=//`pwd`" >> $i.repo
+    echo "enabled=1" >> $i.repo
+    echo "gpgcheck=0" >> $i.repo
+    rm -rf /etc/yum.repos.d/$i.repo
+    cp $i.repo /etc/yum.repos.d/
+    yum install $i -y
+    if [[ $i -eq rocm ]] ; then
+        echo "Creating llvm soft links..."
+        sleep 3
+        for $j in clang ld.lld ; do
+            sudo rm -rf /usr/bin/$i
+            sudo ln -s /opt/rocm/llvm/bin/$i /usr/bin/
+        done
+    fi
     popd
 done
