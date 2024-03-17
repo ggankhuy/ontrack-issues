88b4ce3602c61e244f57481c84e151815a07211b
Your Name <you@example.com>
2024-03-17 08:33:55 -0700

    build.sh: diff build options are added

--- FBA/FBA-336/samples/gpu/build.sh
+++ FBA/FBA-336/samples/gpu/build.sh
@@ -1,1 +1,5 @@
-hipcc -fsanitize=address p61.cpp
+rm *.out
+hipcc -fsanitize=address p61.cpp -o p61.fsanitize.out
+hipcc -fsanitize=address -g  -shared-libsan  p61.cpp -o p61.fsanitize.shared-libsan.g.out
+hipcc -fsanitize=address -g  -shared-libsan --offload-arch=gfx908:xnack+ p61.cpp -o p61.fsanitize.shared-libsan.g.offload.908.out
+ls -l *.out
