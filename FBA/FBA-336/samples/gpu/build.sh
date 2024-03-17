7641a20d5e2cd81008293c20dd7aadcf88e5faa5
Your Name <you@example.com>
2024-03-17 10:21:39 -0700

    build.sh: improved filenaming for output files

--- FBA/FBA-336/samples/gpu/build.sh
+++ FBA/FBA-336/samples/gpu/build.sh
@@ -4,9 +4,11 @@ LOG_DIR=./log/
 mkdir -p $OUT_DIR $LOG_DIR
 rm *.out
 for param in "-fsanitize=address" "-fsanitize=address -shared-libsan" "-fsanitize=address -shared-libsan -g"  ; do
-#   param_no_whitespace=`sed -i '/ /c \\' $param`
-    echo $param_no_whitespace
-    hipcc $param p61.cpp -o $OUT_DIR/p61."$param".out 2>&1 | tee $LOG_DIR/hipcc."$param".log
+    param_no_nonprtable=`echo $param | sed 's/ //g'`
+    param_no_nonprtable=`echo $param_no_nonprtable | sed 's/\-/./g'`
+    param_no_nonprtable=`echo $param_no_nonprtable | sed 's/=/./g'`
+    echo param_no_nonprtable: $param_no_nonprtable
+    hipcc $param p61.cpp -o $OUT_DIR/p61"$param_no_nonprtable".out 2>&1 | tee $LOG_DIR/hipcc"$param_no_nonprtable".log
 done
 ls -l $LOG_DIR/hipcc.*.log
 ls -l $OUT_DIR/*.out
