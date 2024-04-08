a80c316c38e36253fd0516685d9e9edc4a493aac
Your Name <you@example.com>
2024-04-08 18:48:35 +0000

    t1 is transposed but with warning

--- FBA/FBA-336/samples/hipblaslt-torch/ex2.py
+++ FBA/FBA-336/samples/hipblaslt-torch/ex2.py
@@ -1,11 +1,40 @@
+#!/usr/bin/python3
 import torch
+
+'''
+workingdims without T.
 t1=torch.randn(2304, 8, 16384)
 t2=torch.randn(2304, 16384, 8)
+'''
+
+t1=torch.randn(16384, 8, 2304)
+t2=torch.randn(2304, 16384, 8)
+
 print(t1.shape)
 print(t2.shape)
-t3=torch.matmul(t1, t2)
+print("after T:")
+print(t1.T.shape)
+print(t2.shape)
+
+t3=torch.matmul(t1.T, t2)
 print(t3.shape)
 
+'''
+torch.Size([2304, 8, 16384])
+torch.Size([2304, 16384, 8])
+after T:
+/root/extdir/gg/git/fba/FBA/FBA-336/samples/hipblaslt-torch/./ex2.py:9: UserWarning: The use of `x.T` on tensors of dimension other than 2 to reverse their shape is deprecated and it will throw an error in a future release. Consider `x.mT` to transpose batches of matrices or `x.permute(*torch.arange(x.ndim - 1, -1, -1))` to reverse the dimensions of a tensor. (Triggered internally at ../aten/src/ATen/native/TensorShape.cpp:3675.)
+  print(t1.T.shape)
+torch.Size([16384, 8, 2304])
+torch.Size([2304, 16384, 8])
+Traceback (most recent call last):
+'''
+
+# working dims (printout)
+#torch.Size([2304, 8, 16384])
+#torch.Size([2304, 16384, 8])
+
+#torch.Size([2304, 8, 8])
 
 # transpose_mat1 1 
 # transpose_mat2 0 
