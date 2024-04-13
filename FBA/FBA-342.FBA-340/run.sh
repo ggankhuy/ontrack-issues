41c1983dcbf3d46d9ef45257b616359fd716d6fe
Your Name <you@example.com>
2024-04-13 17:51:54 +0000

    FBA-342: rdc pat point to rdc not rdc.ga

--- FBA/FBA-342.FBA-340/run.sh
+++ FBA/FBA-342.FBA-340/run.sh
@@ -1,7 +1,7 @@
-RDC_REPO_ROOT=/root/extdir/gg/git/codelab-scripts/rocm-scripts/rdc/rdc.ga/
+RDC_REPO_ROOT=/root/extdir/gg/git/codelab-scripts/rocm-scripts/rdc/rdc/
 LD_LIBRARY_PATH=$RDC_REPO_ROOT/ras_prebuild/:$RDC_REPO_ROOT/build/rdc_libs/rdc_modules/rdc_rvs:$RDC_REPO_ROOT/build/rdc_libs/rdc_modules/rdc_rocr/ ./a.out
 
 #root@localhost:~/extdir/gg/git/fba/FBA/FBA-342.FBA-340# find ~/extdir/gg/git/codelab-scripts/ -name librdc_ras.so
-#/root/extdir/gg/git/codelab-scripts/rocm-scripts/rdc/rdc.ga/ras_prebuild/librdc_ras.so
+#/root/extdir/gg/git/codelab-scripts/rocm-scripts/rdc/rdc/ras_prebuild/librdc_ras.so
 #root@localhost:~/extdir/gg/git/fba/FBA/FBA-342.FBA-340# find ~/extdir/gg/git/codelab-scripts/ -name librdc_rvs.so
-#/root/extdir/gg/git/codelab-scripts/rocm-scripts/rdc/rdc.ga/build/rdc_libs/rdc_modules/rdc_rvs/librdc_rvs.so
+#/root/extdir/gg/git/codelab-scripts/rocm-scripts/rdc/rdc/build/rdc_libs/rdc_modules/rdc_rvs/librdc_rvs.so
