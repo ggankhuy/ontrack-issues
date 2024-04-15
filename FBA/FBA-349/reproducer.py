586215b4996eac81d2862eeccde892ddb95af5db
Your Name <you@example.com>
2024-04-15 23:14:58 +0000

    reproducer.py: verified working with workload on gpu

--- FBA/FBA-349/reproducer.py
+++ FBA/FBA-349/reproducer.py
@@ -1,7 +1,16 @@
-import amdsmi
+import sys
 
+sys.path.append('/opt/rocm/share/amd_smi/')
+for i in sys.path:
+    print(i)
+
+import amdsmi
 amdsmi.amdsmi_init()
 devices = amdsmi.amdsmi_get_processor_handles()
 processes = amdsmi.amdsmi_get_gpu_process_list(devices[0])
-process_info = amdsmi.amdsmi_get_gpu_process_info(devices[0], processes[0])
-print(process_info)
+try:
+    process_info = amdsmi.amdsmi_get_gpu_process_info(devices[0], processes[0])
+    print(process_info)
+except Exception as msg:
+    print("Make sure workload is running on gpu otherwise processes[0] will None and cause this."
+    print(msg)
