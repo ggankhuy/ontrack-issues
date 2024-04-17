06293337783f167e0aec3a44874fa056878c3122
Your Name <you@example.com>
2024-04-17 10:10:20 -0700

    FBA-349: syntax error fix, commented out processes[0], added ecc count temp

--- FBA/FBA-349/reproducer.py
+++ FBA/FBA-349/reproducer.py
@@ -8,9 +8,12 @@ import amdsmi
 amdsmi.amdsmi_init()
 devices = amdsmi.amdsmi_get_processor_handles()
 processes = amdsmi.amdsmi_get_gpu_process_list(devices[0])
+ecc_count = amdsmi_get_gpu_total_ecc_count()
+'''
 try:
     process_info = amdsmi.amdsmi_get_gpu_process_info(devices[0], processes[0])
     print(process_info)
 except Exception as msg:
-    print("Make sure workload is running on gpu otherwise processes[0] will None and cause this."
+    print("Make sure workload is running on gpu otherwise processes[0] will None and cause this.")
     print(msg)
+'''
