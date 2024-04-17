1da3f6b1f09567f39b5cd16a73ffb67bdaaea525
Your Name <you@example.com>
2024-04-17 13:26:49 -0700

    reproducer-2.py: working

--- FBA/FBA-349/reproducer-2.py
+++ FBA/FBA-349/reproducer-2.py
@@ -1,18 +1,20 @@
 import sys
-
+sys.path.append("/opt/rocm/share/amd_smi/amdsmi/")
 sys.path.append('/opt/rocm/share/amd_smi/')
+#/opt/rocm-6.2.0-13611/share/amd_smi/amdsmi/amdsmi_interface.py
+#from amdsmi_interface import *
 for i in sys.path:
     print(i)
 
 import amdsmi
 amdsmi.amdsmi_init()
 try:
-    devices = amdsmi_get_processor_handles()
+    devices = amdsmi.amdsmi_get_processor_handles()
     if len(devices) == 0:
         print("No GPUs on machine")
     else:
         for device in devices:
-            ecc_error_count = amdsmi_get_gpu_total_ecc_count(device)
+            ecc_error_count = amdsmi.amdsmi_get_gpu_total_ecc_count(device)
             print(ecc_error_count["correctable_count"])
             print(ecc_error_count["uncorrectable_count"])
 except Exception as e:
