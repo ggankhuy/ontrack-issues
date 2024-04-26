74b6948da96d69ba6779cd0854fc3aaff663c8ac
Your Name <you@example.com>
2024-04-26 08:12:36 +0000

    sample.py: much improved code

--- FBA/FBA-349/sample.py
+++ FBA/FBA-349/sample.py
@@ -6,28 +6,42 @@ for i in sys.path:
 
 import amdsmi
 amdsmi.amdsmi_init()
-print("test: amdsmi.amdsmi_get_processor_handles.")
+
+devices = amdsmi.amdsmi_get_processor_handles()
+
 try:
     devices = amdsmi.amdsmi_get_processor_handles()
+
+    print("--- test: amdsmi.amdsmi_get_process_info.")
     processes = amdsmi.amdsmi_get_gpu_process_list(devices[0])
     process_info = amdsmi.amdsmi_get_gpu_process_info(devices[0], processes[0])
     print(process_info)
+
 except Exception as msg:
-    print("Exception: Make sure workload is running on gpu otherwise processes[0] will None and cause this.")
     print(msg)
 
-print("test: amdsmi.amdsmi_get_gpu_total_ecc_count/xgmistat")
-try:
-    devices = amdsmi.amdsmi_get_processor_handles()
-    if not devices:
-        print("No GPUs on machine")
-    else:
+fields=[\
+    ['average_socket_power','gfx_voltage','power_limit'],\
+    ['vram_used','vram_total'],\
+    ['correctable_count','uncorrectable_count']\
+    ]
+
+idx=0
+for i in [\
+    amdsmi.amdsmi_get_power_info, \
+    amdsmi.amdsmi_get_gpu_vram_usage, \
+    amdsmi.amdsmi_get_gpu_total_ecc_count,\
+    amdsmi.amdsmi_gpu_xgmi_error_status\
+    ]:
+
+    print(" ---- TESTING : ", i)
+    try:
         for device in devices:
-            ecc_error_count = amdsmi.amdsmi_get_gpu_total_ecc_count(device)
-            xgmi_stat = amdsmi.amdsmi_gpu_xgmi_error_status(device)
-            print("corr count: (amdsmi.amdsmi_get_gpu_total_ecc_count):     ", ecc_error_count["correctable_count"])
-            print("uncorr count: (amdsmi.amdsmi_get_gpu_total_ecc_count):   ", ecc_error_count["uncorrectable_count"])
-            print("xgmi stat: (amdsmi.amdsmi_gpu_xgmi_error_status):        ", xgmi_stat)
-except Exception as msg:
-    print(msg)
+            result=i(device)
+
+            for j in fields[idx]:
+                print(j, ": ", result[j])
+    except Exception as msg:
+        print(i)
+    idx = idx + 1
 
