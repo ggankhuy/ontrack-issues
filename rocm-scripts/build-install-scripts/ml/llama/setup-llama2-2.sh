a55d433fa5049de1c761b50b328a7d7e298d4ae4
Your Name <you@example.com>
2024-05-03 01:29:13 +0000

    setup-llama2-2.sh: added, setup-llama2.sh: add conda activate line to bashrc to see if works

--- rocm-scripts/build-install-scripts/ml/llama/setup-llama2-2.sh
+++ rocm-scripts/build-install-scripts/ml/llama/setup-llama2-2.sh
@@ -9,20 +9,7 @@ MINICONDA_DIR=/$HOME/miniconda3
 LLAMA_PREREQ_PKGS=20240502_quanta_llamav2
 CONDA=/$HOME/miniconda3/bin/conda
 
-mkdir -p $MINICONDA_SRC_DIR
-wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ./miniconda.sh
-chmod 755 ./miniconda.sh
-bash ./miniconda.sh -b -u -p /$MINICONDA_SRC_DIR
-rm -rf ./miniconda.sh
-echo "export PATH=$PATH:/$MINICONDA_SRC_DIR/bin" >> ~/.bashrc
-ln -s $MINICONDA_SRC_DIR /$HOME/
-
 CONDA_ENV_NAME="llama2"
-$CONDA create --name  $CONDA_ENV_NAME python==3.9 -y
-$CONDA init
-echo "conda activate $CONDA_ENV_NAME" >> ~/.bashrc
-
-exit 0
 
 tar -xvf  $LLAMA_PREREQ_PKGS.tar
 pushd $LLAMA_PREREQ_PKGS
