b3d661aa3bdff503c92bffe067ce8bee093093e1
root <root@gt-pla-u25-08.pla.dcgpu>
2024-06-12 01:51:30 +0000

    FBA-345: run.sh: added

---
+++ FBA/FBA-345/run.sh
@@ -0,0 +1,1 @@
+ LD_LIBRARY_PATH=/root/extdir/gg/git/codelab-scripts/rocm-scripts/rdc/rdc/build/rdc_libs/bootstrap/:/root/extdir/gg/git/codelab-scripts/rocm-scripts/rdc/rdc/build/rdc_libs/rdc/ ./a.out
