479cd3487f37c35e1b07922c84dac1348aa97b0c
Your Name <you@example.com>
2024-06-12 02:01:34 +0000

    FBA-345: run.sh: pcie_test params added from jira

--- FBA/FBA-345/run.sh
+++ FBA/FBA-345/run.sh
@@ -1,1 +1,1 @@
- LD_LIBRARY_PATH=/root/extdir/gg/git/codelab-scripts/rocm-scripts/rdc/rdc/build/rdc_libs/bootstrap/:/root/extdir/gg/git/codelab-scripts/rocm-scripts/rdc/rdc/build/rdc_libs/rdc/ ./a.out
+ LD_LIBRARY_PATH=/root/extdir/gg/git/codelab-scripts/rocm-scripts/rdc/rdc/build/rdc_libs/bootstrap/:/root/extdir/gg/git/codelab-scripts/rocm-scripts/rdc/rdc/build/rdc_libs/rdc/ ./a.out --device 1 --mode=range --htod --iters=50000 --start=100000000 --end=100000000 --increment=1 2>&1 | tee run.log
