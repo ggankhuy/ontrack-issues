ae02629c66990bb0a5fa8a095c11277862252512
Your Name <you@example.com>
2024-08-08 23:02:07 +0000

    rocm-local-install-4.sh: dwarves

--- FBA/FBA-407/repro.sh
+++ FBA/FBA-407/repro.sh
@@ -1,6 +1,7 @@
 sudo mkdir log
-for i in watchdog_fail/CPER_files/*.cper ; do 
-    sudo python3 ./ADDC_CDUMP_Analyzer_INSTINCT_2.1.3/ADDC_CDUMP_Analyzer_INSTINCT/cdump_analyzer.py -i $i 2>&1 | \
+PY_ANALYZER=`find . -name cdump_analyzer.py | head -1`
+for i in ./CPER_files/*.cper ; do 
+    sudo python3 $PY_ANALYZER -i $i 2>&1 | \
     sudo tee ./log/`basename $i`.log ; 
 done
 tree -fs log
